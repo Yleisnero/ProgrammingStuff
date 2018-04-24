0838ca942233bb35f06706e5a8a3fa114b5c4707
Yleisnero <36032173+Yleisnero@users.noreply.github.com>
2018-04-24 16:45:31 +0200

    Add files via upload

---
+++ hallo.cpp
@@ -0,0 +1,7 @@
+#include "std_lib_facilities.h"
+
+int main(){
+	cout << "Hallo Welt" << endl;
+	cout << "jo" << endl;
+	return 0;
+}
---
+++ servus.cpp
@@ -0,0 +1,6 @@
+#include "std_lib_facilities2.h"
+
+int main(){
+cout << "Servus" << endl;
+return 0;
+}
