cf2e9c834204bf91e85cb592180c1a1266db7418
Yleisnero <36032173+Yleisnero@users.noreply.github.com>
2018-05-08 13:12:54 +0200

    Add files via upload

--- std_lib_facilities.h
+++ std_lib_facilities.h
@@ -19,27 +19,6 @@
 	Revised November 25 2013: remove support for pre-C++11 compilers, use C++11: <chrono>
 	Revised November 28 2013: add a few container algorithms
 	Revised June 8 2014: added #ifndef to workaround Microsoft C++11 weakness
-/*
-   std_lib_facilities.h
-*/
-
-/*
-	simple "Programming: Principles and Practice using C++ (second edition)" course header to
-	be used for the first few weeks.
-	It provides the most common standard headers (in the global namespace)
-	and minimal exception/error support.
-
-	Students: please don't try to understand the details of headers just yet.
-	All will be explained. This header is primarily used so that you don't have
-	to understand every concept all at once.
-
-	By Chapter 10, you don't need this file and after Chapter 21, you'll understand it
-
-	Revised April 25, 2010: simple_error() added
-	
-	Revised November 25 2013: remove support for pre-C++11 compilers, use C++11: <chrono>
-	Revised November 28 2013: add a few container algorithms
-	Revised June 8 2014: added #ifndef to workaround Microsoft C++11 weakness
 */
 
 #ifndef H112
