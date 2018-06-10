35cb4d2c2f7b3a96939eccd1a910c44f71662c12
Yleisnero <36032173+Yleisnero@users.noreply.github.com>
2018-06-10 20:23:00 +0000

    Update hrmDequeSTL.cpp

--- hrmDequeSTL.cpp
+++ hrmDequeSTL.cpp
@@ -2,7 +2,8 @@
 #include <deque> 
 using namespace std;
 
-void printKMax(int arr[], int n, int k){ //WATCH OUT: THIS WAY ISN´T WORKING FOR BIG ARRAYS .... :(
+void printKMax(int arr[], int n, int k){ //WATCH OUT: THIS WAY ISN´T WORKING FOR BIG ARRAYS .... :( Use this instead: 
+					https://www.geeksforgeeks.org/sliding-window-maximum-maximum-of-all-subarrays-of-size-k/#disqus_thread
 	//Write your code here.
     int max;
     for(int i = 0; i <= n-k; i++){ //count of array = n
