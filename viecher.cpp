d989c4aed1862542a1789cca18306094a5e70f28
Yleisnero <36032173+Yleisnero@users.noreply.github.com>
2018-06-19 13:17:19 +0200

    Add files via upload

--- viecher.cpp
+++ viecher.cpp
@@ -1,16 +1,27 @@
 #include <iostream>
 #include <string>
 
+enum struct Gattung {Gliedertier = 1, Vogel, Saeugetier, Weichtier, Fisch, Kriechtier, Spinne};
+
 class tier{
 
 protected: std::string name;
 	   int anzahlBeine;
+	   Gattung gattung;
 
-public: tier(std::string nameN, int anzahlBeineN = 4) : name(nameN), anzahlBeine(anzahlBeineN){ }
+public: tier(std::string nameN, Gattung gattungN, int anzahlBeineN = 4) : name(nameN), gattung(gattungN), anzahlBeine(anzahlBeineN){ }
 	
-	void beschreibungAusgeben(){
+	virtual void beschreibungAusgeben(){
 		std::cout << "Ich heiße " << name << " und habe " << anzahlBeine << " Beine"
 		<< std::endl;
+	}
+
+	virtual void paarung(tier t){
+		if(gattung == t.gattung){
+			std::cout << "Erfolgreiche Paarung von " << name << " und " << t.name << std::endl; 		
+		}else{
+			std::cout << "Keine erfolgreiche Paarung! " << std::endl; 
+		}
 	}
 };
 
@@ -18,7 +29,7 @@ class insekt : public tier{
 
 private: int anzahlFluegel;
 
-public: insekt(std::string nameN, int anzahlFluegelN, int anzahlBeineN = 4) : tier(nameN, anzahlBeineN), 		 			anzahlFluegel(anzahlFluegelN){ }
+public: insekt(std::string nameN, Gattung gattungN, int anzahlFluegelN, int anzahlBeineN = 4) : tier(nameN, gattungN, anzahlBeineN), anzahlFluegel(anzahlFluegelN){ }
 	
 	bool kannFliegen(){
 		if(anzahlFluegel > 1){
@@ -28,29 +39,48 @@ public: insekt(std::string nameN, int anzahlFluegelN, int anzahlBeineN = 4) : ti
 		}
 	}
 
-	virtual void beschreibungAusgeben(){
+	void beschreibungAusgeben(){
 		std::cout << "Ich heiße " << name << " und habe " << anzahlBeine << " Beine "
 		<< "und habe " << anzahlFluegel << " Fluegel" << std::endl;
+	}
+
+	void paarung(tier t){
+		if(gattung == t.gattung){
+			if(gattung == Gattung::Spinne){
+				std::cout << "Erfolgreiche Paarung von " << name << " und " << t.name << std::endl;
+				std::cout << name << " frisst " << t.name << std::endl;
+}else{
+			std::cout << "Erfolgreiche Paarung von " << name << " und " << t.name << std::endl;
+} 		
+		}else{
+			std::cout << "Keine erfolgreiche Paarung! " << std::endl; 
+		}
 	}
 
 };
 
 
 int main(){
-	tier t1("Eumel", 42);
+	tier t1("Eumel", Gattung::Weichtier, 42);
 	t1.beschreibungAusgeben();
 
-	tier t2("Hundi");
+	tier t2("Hundi", Gattung::Weichtier);
 	t2.beschreibungAusgeben();
 
-	insekt i1("spiderschwein", 0, 8);
+	
+	insekt i1("spiderschwein", Gattung::Spinne, 0, 8);
 	i1.beschreibungAusgeben();
 
+	insekt i2("spinneee", Gattung::Spinne, 0, 7);
+	i1.paarung(i2);
+
 	if(i1.kannFliegen()){
-		std::cout << "kann fliegen!!!" << std::endl;
+		std::cout << "Ich kann fliegen!!!" << std::endl;
 	}else{
-		std::cout << "kann nicht fliegen :(" << std::endl;
+		std::cout << "Ich kann nicht fliegen :(" << std::endl;
 	}
+
+	t1.paarung(t2);
 
 return 0;
 }
