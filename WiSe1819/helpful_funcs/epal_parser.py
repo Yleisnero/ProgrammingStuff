69442b485fd8765bf75703360b6664293504bd52
liquidiert <36642878+liquidiert@users.noreply.github.com>
2018-11-21 14:46:17 +0100

    Update epal_parser.py

--- WiSe1819/helpful_funcs/epal_parser.py
+++ WiSe1819/helpful_funcs/epal_parser.py
@@ -3,7 +3,7 @@ Parser for epal, your friendly, new programming language!
 epal code is easy to write and understand!
 This parser parses epal code to c++ code and compiles it to binary
 It also can be executed as pure python module
-When called in CLI command should be like following example:
+When called in CLI, command should be like following example:
 python [epal_file] [binary_filename]
 """
 import sys
